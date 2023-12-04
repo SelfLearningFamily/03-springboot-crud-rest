0c13bbac6d28a6c85b8b93d37f3bbb45ef456196
msiddiqui <msiddiqui@i2cinc.com>
2023-12-05 02:03:17 +0500

    Springboot curd-rest: Exception Handling II
    covers the edge case that mentioned in the last commit. there is also some details mentioned above a method. please go through it too

--- src/main/java/com/zbrickx/crudrest01/rest/StudentRestController.java
+++ src/main/java/com/zbrickx/crudrest01/rest/StudentRestController.java
@@ -47,4 +47,16 @@ public class StudentRestController {
         ser.setTimestamp(System.currentTimeMillis());
         return new ResponseEntity<>(ser,HttpStatus.NOT_FOUND);
     }
+
+    // not very good practice to handle multiple exception in one method. But for now consider it as an outer
+    //barrier to make our system more robust
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exe){
+        StudentErrorResponse ser = new StudentErrorResponse();
+        ser.setMessage(exe.getMessage());
+        ser.setStatus(HttpStatus.BAD_REQUEST.value());
+        ser.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(ser,HttpStatus.BAD_REQUEST);
+    }
+
 }
